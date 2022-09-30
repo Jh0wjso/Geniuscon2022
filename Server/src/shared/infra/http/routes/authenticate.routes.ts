a5458655e755730209a4365fad5574f6e802e4ac
Felipe Henrique Camargos Ferreira <felipeferreira.sp.dev@gmail.com>
2022-09-30 17:24:53 -0300

    feat: criando autenticação de usuário

---
+++ Server/src/shared/infra/http/routes/authenticate.routes.ts
@@ -0,0 +1,13 @@
+import { AuthenticateUserController } from "@modules/accounts/useCases/authenticateUser/AuthenticateUserController";
+import { RefreshTokenController } from "@modules/accounts/useCases/refreshToken/RefreshTokenController";
+import { Router } from "express";
+
+const authenticateRoutes = Router();
+
+const authenticateUserController = new AuthenticateUserController();
+const refreshTokenController = new RefreshTokenController();
+
+authenticateRoutes.post("/sessions", authenticateUserController.handle);
+authenticateRoutes.post("/refresh-token", refreshTokenController.handle);
+
+export { authenticateRoutes };
