3a457bfe59c4e77ce623e0c2235edc91e20f7d28
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2022-09-30 19:27:50 -0300

    :lipstick: feat add styles in the footer

--- Client/src/components/Footer/index.tsx
+++ Client/src/components/Footer/index.tsx
@@ -4,81 +4,80 @@ import './styles.css'
 import logo from '/src/assets/img/logos/ICON.png'
 import logoBanner from '/src/assets/img/logos/logo-banner.png'
 import { Link } from 'react-router-dom'
+import { BsSquare } from 'react-icons/bs'
 
 export function Footer() {
   return (
     <footer className="border-t-2">
-      <div className="footer">
-        <div className="redes">
-          <div className="logo">
-            <img
-              className="w-auto mt-6 mb-6 object-contain"
-              src={logo}
-              alt="Logo agrolugue"
-            />
-          </div>
-          <div className="redesIcon">
-            <a href="google.com">
-              <FaFacebook size={28} />
-            </a>
-            <a href="">
-              <FaYoutube size={28} />
-            </a>
-          </div>
-        </div>
-        <div className="containerHero">
-          <div className="containerInfos">
-            <div className="column titleHidden">
-              <h3></h3>
-              <div className="pages">
-                <Link to="/historia"></Link>
-                <Link to="/hoteis"></Link>
-                <Link to="/turismo"></Link>
-                <Link to="/map"></Link>
-              </div>
+      <div className="footer bg-gray-550 ">
+      
+        <div className='flex'>
+          <div className="redes mr-4">
+            <img src={logoBanner} alt="" />
+              <div className="redesIcon ">
+                <a href="google.com">
+                  <FaFacebook size={28}/>
+                </a>
+                <a href="">
+                  <FaYoutube size={28} />
+                </a>
+            
             </div>
-            <div className="column titleHidden">
-              <h3>Agenda</h3>
-              <div className="notices">
-                <ul>
-                  <li>
-                     texto
-                  </li>
-                  <li>
-                    texto
-                  </li>
-                  <li>
-                    texto
-                  </li>
-                </ul>
+          </div>
+          <div className="containerHero pt-8">
+            <div className="containerInfos bg-gray-550 ">
+              <div className="column titleHidden text-white">
+                <h3 className='text-white'>Navegação</h3>
+                <div className="pages">
+                  <Link to="#">Início</Link>
+                  <Link to="#"> Serviços</Link>
+                  <Link to="#">Mapas</Link>
+                  <Link to="#"></Link>
+                </div>
               </div>
-            </div>
-            <div className="column">
-              <h3></h3>
+              <div className="column titleHidden">
+                <h3 className='text-white'>Atualização</h3>
+                <div className="notices text-white">
+                  <ul>
+                    <li>
+                      texto
+                    </li>
+                    <li>
+                      texto
+                    </li>
+                    <li>
+                      texto
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            <div className="column text-white">
+              <h3 className='text-white'>Contato</h3>
               <div className="contacts">
-                <strong>Rua Frei Raphael Proner, 1457</strong>
+                <strong>Universidade Estadual do Norte do Paraná</strong>
                 <strong>
-                  <FaPhone color="#727272" />
-                  (43) 3542-4525
+                  <FaPhone color="" />
+                  (43) 0000-0000
                 </strong>
                 <strong>
-                  <FaEnvelope color="#727272" />
-                  comunicacao@bandeirantes.pr.gov.br
+                  <FaEnvelope color="" />
+                  emailImaginario@uenp.edu.br
                 </strong>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="copyright">
-        <p></p>
-        <img
-          className="w-auto max-h-20 bg-contain"
-          src={logoBanner}
-          alt="Logo Bandeirantes"
-        />
-        <p></p>
-      </div>
+        </div>
+        <div className="copyright bg-gray-550 text-white">
+          <p>© 2022 - Genius Hackathon - Todos os direitos reservados</p>
+          <img
+            className=" w-auto max-h-20 bg-contain"
+            src={logo}
+            alt="Logo Bandeirantes"
+          />
+          <p>Website desenvolvido pela equipe Garage Sprint Code</p>
+        </div>
+        </div>
     </footer>
   )
 }
