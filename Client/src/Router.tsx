9b218dec97f5808c2c2184f4ce9d0192b2c253ff
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2022-09-30 21:07:06 -0300

    :wrench: feat rotas e tailwind

--- Client/src/Router.tsx
+++ Client/src/Router.tsx
@@ -1,9 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
+import Cadastro from './screens/Dashboard/Cadastro'
+import Login from './screens/Dashboard/Login'
 
 export function Router() {
   return (
     <Routes>
-      
+      <Route path="/login" element={<Login/>} />
+      <Route path="/cadastro" element={<Cadastro/>} />
     </Routes>
   )
 }
