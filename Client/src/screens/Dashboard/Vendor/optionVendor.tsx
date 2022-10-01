48b0ce488d1929a72cfada3c8e077047be414559
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2022-10-01 17:46:20 -0300

    :lipstick: feat add dashboard vendor com components

---
+++ Client/src/screens/Dashboard/Vendor/optionVendor.tsx
@@ -0,0 +1,40 @@
+import { FiUsers } from 'react-icons/fi';
+import { TbMailbox } from 'react-icons/tb';
+import { FiList } from 'react-icons/fi';
+import { FiMap } from 'react-icons/fi';
+import { FiMessageSquare } from 'react-icons/fi';
+import { FiStar } from 'react-icons/fi';
+
+export const optionVendor = [
+  {
+    titulo: 'Clientes',
+    estado: '0',
+    icone: FiUsers
+  },
+  {
+    titulo: 'Caixa de entrada',
+    estado: '0',
+    icone: TbMailbox
+  },
+  {
+    titulo: 'Lista de Produtos',
+    estado: '0',
+    icone: FiList
+  },
+  {
+    titulo: 'Mapa',
+    estado: '0',
+    icone: FiMap
+  },
+  {
+    titulo: 'Mensagens',
+    estado: '0',
+    icone: FiMessageSquare
+  },
+  {
+    titulo: 'Avaliações',
+    estado: '0',
+    icone: FiStar
+
+  }
+]
