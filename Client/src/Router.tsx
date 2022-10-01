c843a4a7c35a2da9d13deea367f304d988009983
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2022-10-01 17:49:03 -0300

    :wrench: chore add rotas

--- Client/src/Router.tsx
+++ Client/src/Router.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Cadastro from './screens/Dashboard/Cadastro'
 import Login from './screens/Dashboard/Login'
+import User from './screens/Dashboard/User'
 import Vendor from './screens/Dashboard/Vendor'
 
 export function Router() {
@@ -10,6 +11,7 @@ export function Router() {
       <Route path="/login" element={<Login/>} />
       <Route path="/cadastro" element={<Cadastro/>} />
       <Route path="/vendor" element={<Vendor/>} />
+      <Route path="/cliente" element={<User/>} />
     </Routes>
   )
 }
