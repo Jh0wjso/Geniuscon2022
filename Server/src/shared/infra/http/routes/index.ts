44208d1126ba989b2668d213d21ef26b7120b518
Felipe Henrique Camargos Ferreira <felipeferreira.sp.dev@gmail.com>
2022-10-02 00:01:09 -0300

    feat: configurado rota de ofertas

--- Server/src/shared/infra/http/routes/index.ts
+++ Server/src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { authenticateRoutes } from "./authenticate.routes";
+import { offersRoutes } from "./offers.routes";
 import { passwordRoutes } from "./password.routes";
 import { usersRoutes } from "./users.routes";
 
@@ -8,6 +9,7 @@ const router = Router();
 
 router.use("/users", usersRoutes);
 router.use("/password", passwordRoutes);
+router.use("/offers", offersRoutes);
 router.use(authenticateRoutes);
 
 export { router };
