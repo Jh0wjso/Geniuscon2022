098d259128a2cd04aae66e81807c988c045c2a2b
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2022-10-03 01:31:19 -0300

    :lipstick: feat added Calendar

--- Client/src/components/CardsDashboard/index.tsx
+++ Client/src/components/CardsDashboard/index.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Calendar } from 'react-calendar';
+import Dashboard from '../Dashboard';
 import './styles.css';
+import './calendarStyles.css';
+import 'react-calendar/dist/Calendar.css';
 
 interface ObjectItem {
   titulo: string;
@@ -12,22 +16,34 @@ interface CardsDashboardProps {
 }
 
 export function CardsDashboard({ objectList }: CardsDashboardProps) {
+  const [date, setDate] = useState(new Date());
 
   return (
     <div className="vendor-body">
-      <div className="vendor-content">
-        {objectList.map((data: ObjectItem) =>
-          <div className="vendor-card">
-            <div className="vendor-card-content">
-              <data.icone size={30} />
-              <h1>{data.titulo}</h1>
-              <h4>{data.estado}</h4>
+      <Dashboard/>
+      <div className="vendor-container">
+        <div className='vendor-content'>
+            {objectList.map((data: ObjectItem) =>
+              <div className="vendor-card">
+                <div className="vendor-card-content">
+                  <data.icone size={30} />
+                  <h1>{data.titulo}</h1>
+
+                  <h4>{data.estado}</h4>
+                </div>
+              </div>
+            )}
+          </div>
+          <div className='flex justify-center'> 
+            <div className="vendor-calendar">
+                <div className='calendar-content'>
+                  <div className='flex justify-center mt-2 mb-2 text-base text-zinc-500 '>
+                    <a>Calendário</a>
+                  </div>
+                  <Calendar calendarType='Hebrew' onChange={setDate} value={date}/>
+                </div>
             </div>
           </div>
-        )}
-      </div>
-      <div className="vendor-calendar">
-        <h1>Calendário</h1>
       </div>
     </div>
   )
