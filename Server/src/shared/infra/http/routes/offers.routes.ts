ee25f88788404ea19f9a9d3643f403aaac674777
Felipe Henrique Camargos Ferreira <felipeferreira.sp.dev@gmail.com>
2022-10-04 20:08:37 -0300

    feat: adicionado rota

--- Server/src/shared/infra/http/routes/offers.routes.ts
+++ Server/src/shared/infra/http/routes/offers.routes.ts
@@ -1,15 +1,24 @@
+import uploadConfig from "@config/upload";
 import { CreateOfferController } from "@modules/offers/useCases/createOffer/CreateOfferController";
 import { ListAllOffersController } from "@modules/offers/useCases/listAllOffers/ListAllOffersController";
 import { Router } from "express";
+import multer from "multer";
 
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const offersRoutes = Router();
 
+const uploadOfferImages = multer(uploadConfig.upload("./tmp/offerImages"));
+
 const createOfferController = new CreateOfferController();
 const listAllOffersController = new ListAllOffersController();
 
-offersRoutes.post("/", ensureAuthenticated, createOfferController.handle);
+offersRoutes.post(
+  "/",
+  ensureAuthenticated,
+  uploadOfferImages.array("offerImages"),
+  createOfferController.handle
+);
 offersRoutes.get("/list", listAllOffersController.handle);
 
 export { offersRoutes };
