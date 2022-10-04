bdd719b7ac59739a153dca4543dd414f9687f51a
Gabriel Lima <42157830+Gabriellimmaa@users.noreply.github.com>
2022-10-04 15:53:29 -0300

    :lipstick: feat: Estrutura /Locacao

--- Client/src/Router.tsx
+++ Client/src/Router.tsx
@@ -10,15 +10,15 @@ import Servico from './screens/Servicos'
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Inicio/>}/>
-      <Route path="/locacao" element={<Locacao/>} />
-      <Route path="/servicos" element={<Servico/>} />
+      <Route path="/" element={<Inicio />} />
+      <Route path="/locacao" element={<Locacao />} />
+      <Route path="/servicos" element={<Servico />} />
 
       {/* Dashboards */}
-      <Route path="/login" element={<Login/>} />
-      <Route path="/cadastro" element={<Cadastro/>} />
-      <Route path="/vendor" element={<Vendor/>} />
-      <Route path="/cliente" element={<User/>} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/cadastro" element={<Cadastro />} />
+      <Route path="/vendor" element={<Vendor />} />
+      <Route path="/cliente" element={<User />} />
     </Routes>
   )
 }
--- Client/src/screens/Locacao/index.tsx
+++ Client/src/screens/Locacao/index.tsx
@@ -1,82 +1,98 @@
 import Card from '../../components/Card'
+import { Filter } from '../../components/Filter'
 import './styles.css'
 
 export default function Locacao() {
   return (
-    <section id="locacao">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-1 justify-center gap-5 ">
-        <Card
-          title="Nome do trator"
-          img="https://cdn.discordapp.com/attachments/1026681318618181684/1026871307477975113/unknown.png"
-          preco={100}
-          categoria="Colheitadeira"
-          proprietario="Fazenda São João"
-          marca="John Deere"
+    <section id="locacao" className="p-0">
+      <div className="grid grid-cols-5 gap-5" style={{ height: '90vh' }}>
+        <Filter
+          title="Serviços"
+          opcoes={[
+            'Colheitadeiras',
+            'Forrageira',
+            'Tratores',
+            'Colhedoras',
+            'Pulverizadores',
+            'Grades',
+            'Enfardadoras',
+            'Semeadoras',
+          ]}
         />
-        <Card
-          title="Nome do trator"
-          img="https://cdn.discordapp.com/attachments/1026681318618181684/1026871307477975113/unknown.png"
-          preco={100}
-          categoria="Colheitadeira"
-          proprietario="Fazenda São João"
-          marca="John Deere"
-        />
-        <Card
-          title="Nome do trator"
-          img="https://cdn.discordapp.com/attachments/1026681318618181684/1026871307477975113/unknown.png"
-          preco={100}
-          categoria="Colheitadeira"
-          proprietario="Fazenda São João"
-          marca="John Deere"
-        />
-        <Card
-          title="Nome do trator"
-          img="https://cdn.discordapp.com/attachments/1026681318618181684/1026871307477975113/unknown.png"
-          preco={100}
-          categoria="Colheitadeira"
-          proprietario="Fazenda São João"
-          marca="John Deere"
-        />
-        <Card
-          title="Nome do trator"
-          img="https://cdn.discordapp.com/attachments/1026681318618181684/1026871307477975113/unknown.png"
-          preco={100}
-          categoria="Colheitadeira"
-          proprietario="Fazenda São João"
-          marca="John Deere"
-        />
-        <Card
-          title="Nome do trator"
-          img="https://cdn.discordapp.com/attachments/1026681318618181684/1026871307477975113/unknown.png"
-          preco={100}
-          categoria="Colheitadeira"
-          proprietario="Fazenda São João"
-          marca="John Deere"
-        />
-        <Card
-          title="Nome do trator"
-          img="https://cdn.discordapp.com/attachments/1026681318618181684/1026871307477975113/unknown.png"
-          preco={100}
-          categoria="Colheitadeira"
-          proprietario="Fazenda São João"
-          marca="John Deere"
-        />
-        <Card
-          title="Nome do trator"
-          img="https://cdn.discordapp.com/attachments/1026681318618181684/1026871307477975113/unknown.png"
-          preco={100}
-          categoria="Colheitadeira"
-          proprietario="Fazenda São João"
-          marca="John Deere"
-        />
-        <Card
-          title="Nome do trator"
-          img="https://cdn.discordapp.com/attachments/1026681318618181684/1026871307477975113/unknown.png"
-          preco={100}
-          categoria="Colheitadeira"
-          proprietario="Fazenda São João"
-          marca="John Deere"
-        />
+        <div className="grid scroll-syles col-span-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-1 justify-center gap-5 p-5 overflow-y-auto">
+          <Card
+            title="Nome do trator"
+            img="https://cdn.discordapp.com/attachments/1026681318618181684/1026871307477975113/unknown.png"
+            preco={100}
+            categoria="Colheitadeira"
+            proprietario="Fazenda São João"
+            marca="John Deere"
+          />
+          <Card
+            title="Nome do trator"
+            img="https://cdn.discordapp.com/attachments/1026681318618181684/1026871307477975113/unknown.png"
+            preco={100}
+            categoria="Colheitadeira"
+            proprietario="Fazenda São João"
+            marca="John Deere"
+          />
+          <Card
+            title="Nome do trator"
+            img="https://cdn.discordapp.com/attachments/1026681318618181684/1026871307477975113/unknown.png"
+            preco={100}
+            categoria="Colheitadeira"
+            proprietario="Fazenda São João"
+            marca="John Deere"
+          />
+          <Card
+            title="Nome do trator"
+            img="https://cdn.discordapp.com/attachments/1026681318618181684/1026871307477975113/unknown.png"
+            preco={100}
+            categoria="Colheitadeira"
+            proprietario="Fazenda São João"
+            marca="John Deere"
+          />
+          <Card
+            title="Nome do trator"
+            img="https://cdn.discordapp.com/attachments/1026681318618181684/1026871307477975113/unknown.png"
+            preco={100}
+            categoria="Colheitadeira"
+            proprietario="Fazenda São João"
+            marca="John Deere"
+          />
+          <Card
+            title="Nome do trator"
+            img="https://cdn.discordapp.com/attachments/1026681318618181684/1026871307477975113/unknown.png"
+            preco={100}
+            categoria="Colheitadeira"
+            proprietario="Fazenda São João"
+            marca="John Deere"
+          />
+          <Card
+            title="Nome do trator"
+            img="https://cdn.discordapp.com/attachments/1026681318618181684/1026871307477975113/unknown.png"
+            preco={100}
+            categoria="Colheitadeira"
+            proprietario="Fazenda São João"
+            marca="John Deere"
+          />
+          <Card
+            title="Nome do trator"
+            img="https://cdn.discordapp.com/attachments/1026681318618181684/1026871307477975113/unknown.png"
+            preco={100}
+            categoria="Colheitadeira"
+            proprietario="Fazenda São João"
+            marca="John Deere"
+          />
+          <Card
+            title="Nome do trator"
+            img="https://cdn.discordapp.com/attachments/1026681318618181684/1026871307477975113/unknown.png"
+            preco={100}
+            categoria="Colheitadeira"
+            proprietario="Fazenda São João"
+            marca="John Deere"
+          />
+        </div>
       </div>
     </section>
   )
