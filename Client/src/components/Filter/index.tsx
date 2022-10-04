9e924c3bc0cc0d2f4ab2ad30ad1729a91ee422b5
Gabriel Lima <42157830+Gabriellimmaa@users.noreply.github.com>
2022-10-04 15:53:14 -0300

    :lipstick: style: Filter

--- Client/src/components/Filter/index.tsx
+++ Client/src/components/Filter/index.tsx
@@ -1,5 +1,4 @@
 import './styles.css'
-import { BiMap } from 'react-icons/bi'
 
 interface FilterProps {
   title: string
@@ -8,34 +7,92 @@ interface FilterProps {
 
 export function Filter({ title, opcoes }: FilterProps) {
   return (
-    <div className="filter">
+    <div className="filter overflow-y-auto scroll-syles">
       <div className="filter-body">
         <form action="" method="post">
           <div className="opcoes">
-            <h5 className="filter-title">Pesquisa</h5>
-            <ul>
-              <li>
+            <div className="filter-search">
+              <h5 className="filter-title">Pesquisa</h5>
+              <span className="relative text-gray-400">
+                <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+                  <button
+                    type="submit"
+                    className="p-1 focus:outline-none focus:shadow-outline"
+                  >
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      viewBox="0 0 24 24"
+                      className="w-6 h-6"
+                    >
+                      <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                    </svg>
+                  </button>
+                </span>
                 <input
-                  id="filter-search"
-                  type="text"
-                  placeholder="Digite a região"
+                  type="search"
+                  name="q"
+                  className="py-2 text-sm text-white rounded-md pl-10 focus:outline-none focus:bg-white focus:text-gray-900"
+                  placeholder="Digite a região..."
+                  autoComplete="off"
                 />
-                <button id="filter-button" type="submit">
-                  <BiMap size={24} />
-                </button>
-              </li>
-            </ul>
-            <h5 className="filter-title">{title}</h5>
-            <ul>
-              {opcoes.map((opcao) => {
-                return (
-                  <li key={opcao}>
-                    <input type="checkbox" />
-                    <label>{opcao}</label>
-                  </li>
-                )
-              })}
-            </ul>
+              </span>
+            </div>
+            <div>
+              <h5 className="filter-title">{title}</h5>
+              <ul>
+                {opcoes.map((opcao) => {
+                  return (
+                    <li key={opcao}>
+                      <input type="checkbox" />
+                      <label>{opcao}</label>
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+            <div>
+              <h5 className="filter-title">{title}</h5>
+              <ul>
+                {opcoes.map((opcao) => {
+                  return (
+                    <li key={opcao}>
+                      <input type="checkbox" />
+                      <label>{opcao}</label>
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+            <div>
+              <h5 className="filter-title">{title}</h5>
+              <ul>
+                {opcoes.map((opcao) => {
+                  return (
+                    <li key={opcao}>
+                      <input type="checkbox" />
+                      <label>{opcao}</label>
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+            <div>
+              <h5 className="filter-title">{title}</h5>
+              <ul>
+                {opcoes.map((opcao) => {
+                  return (
+                    <li key={opcao}>
+                      <input type="checkbox" />
+                      <label>{opcao}</label>
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
           </div>
         </form>
       </div>
