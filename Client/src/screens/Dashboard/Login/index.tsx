d86ae37bc08f4263349cb14f33871f2511cd81ed
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2022-10-05 15:54:52 -0300

    :lipstick: style: Login

--- Client/src/screens/Dashboard/Login/index.tsx
+++ Client/src/screens/Dashboard/Login/index.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import { FaFacebookF, FaGoogle } from 'react-icons/fa'
 import './styles.css'
 
 export default function Login() {
@@ -8,17 +9,30 @@ export default function Login() {
         <div className="login-content">
           <form action="" method="post">
             <h1>Login🌱</h1>
-            <h3>Bem-vindo ao Login👋</h3>
-            <p>Por favor, insira email e senha</p>
+            <h3>Bem-vindo</h3>
+            <br />
             <label>Email:</label>
-            <input type="text" placeholder="Usuário" />
+            <input type="text" placeholder="Usuário" className="mb-2" />
             <label>Senha:</label>
             <input type="password" placeholder="Senha" />
-            <input type="submit" value="Entrar" />
-            <p>Possui cadastro?</p>
-            <Link to="/cadastro" className="text-green-600">
-              Cadastre-se
-            </Link>
+            <button className="mt-8">Entrar</button>
+            <div className="text-center">
+              <div className="my-3">
+                <Link to="/cadastro" className="text-green-600">
+                  Esqueceu a senha?
+                </Link>
+              </div>
+
+              <div className="redes-sociais grid grid-cols-2 gap-5">
+                <button>
+                  <FaFacebookF size={30} /> Facebook
+                </button>
+                <button>
+                  <FaGoogle size={30} />
+                  Google
+                </button>
+              </div>
+            </div>
           </form>
         </div>
       </div>
