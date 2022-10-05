9c3d9ff96512b613c47d7af2e5dad3fae4c05275
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2022-10-04 21:51:28 -0300

    :lipstick: feat banner

--- Client/src/screens/Inicio/index.tsx
+++ Client/src/screens/Inicio/index.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable prettier/prettier */
 import { FaSeedling, FaSprayCan, FaStore, FaTruckLoading } from 'react-icons/fa'
 import { FiTarget } from 'react-icons/fi'
 import { GiBrazil, GiFarmTractor, GiSugarCane } from 'react-icons/gi'
 import { MdAgriculture } from 'react-icons/md'
 import { TbBulldozer, TbWorld } from 'react-icons/tb'
+import farm from '../../assets/img/farm.jpeg'
 
 import CardsAcesso from '../../components/CardsAcesso'
 import './styles.css'
@@ -10,7 +12,53 @@ import './styles.css'
 export default function Inicio() {
   return (
     <>
-      <section id="inicio">
+      <img className="w-full h-96 object-cover" src={farm} alt="" />
+      <section id="inicio" className=''>
+          <div className=" w-full h-14 md:w-3/4 m-auto bg-gray-500 text-white  rounded">
+            <div className='md:flex md:justify-around'>
+            <div>
+              <a href="#" className="flex items-center mt-1">
+                <img
+                  className="h-5 w-5 md:h-[50px] md:w-[50px]"
+                  src="https://www.deere.com.br/assets/images/common/home-page/qt-tractor-white.png"
+                  alt=""
+                />
+                <span>Ver equipamentos</span>
+              </a>
+            </div>
+            <div>
+              <a href="#" className="flex items-center mt-1">
+                <img
+                  className="h-5 w-5 md:h-[50px] md:w-[50px]"
+                  src="https://www.deere.com.br/assets/images/common/home-page/qt-dealer-small-wt.png"
+                  alt=""
+                />
+                <span>Onde comprar</span>
+              </a>
+            </div>
+            <div>
+              <a href="#" className="flex items-center mt-1">
+                <img
+                  className="h-5 w-5 md:h-[50px] md:w-[50px]"
+                  src="https://www.deere.com.br/assets/images/common/home-page/qt-book-small-wt.png"
+                  alt=""
+                />
+                <span>Manual técnico</span>
+              </a>
+            </div>
+            <div>
+              <a href="#" className="flex items-center mt-1">
+                <img
+                  className="md:h-[50px] md:w-[50px]"
+                  src="https://www.deere.com.br/assets/images/common/home-page/qt-news-small-wt.png"
+                  alt=""
+                />
+                <span>Notícias</span>
+              </a>
+            </div>
+            </div>
+          </div>
+
         <h1 className="text-center text-6xl">Locação</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-1 justify-center gap-5 ">
           <CardsAcesso
