a2fbf171e28430940e2a1ba1ad86d0fd35752146
Felipe Henrique Camargos Ferreira <felipeferreira.sp.dev@gmail.com>
2022-10-06 09:55:02 -0300

    feat: ligando db com locacoes

--- Client/src/screens/Locacao/index.tsx
+++ Client/src/screens/Locacao/index.tsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from 'react'
 import Card from '../../components/Card'
 import { Filter } from '../../components/Filter'
+import api from '../../services/api'
 import './styles.css'
 
+interface LocacaoProps {
+  id: number
+  nome: string
+  categoria: string
+  marca: string
+  fazenda: string
+  valor: number
+  imagem: string
+}
+
 export default function Locacao() {
+  const [locacao, setLocacao] = useState<LocacaoProps[]>([])
+
+  useEffect(() => {
+    api.get('/locacao').then((response) => {
+      setLocacao(response.data)
+    })
+  }, [])
+
   return (
     <section id="locacao" className="p-0">
       <div className="grid grid-cols-5 gap-5" style={{ height: '90vh' }}>
@@ -21,19 +41,19 @@ export default function Locacao() {
           ]}
         />
         <div className="grid scroll-syles col-span-5 lg:col-span-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-1 justify-center gap-5 p-5 overflow-y-auto">
-          {Array(8)
-            .fill(0)
-            .map((_, i) => (
+          {locacao.map((item) => {
+            return (
               <Card
-                key={i}
-                title="A950"
-                img="https://cdn.discordapp.com/attachments/1026681318618181684/1026871307477975113/unknown.png"
-                preco={100}
-                categoria="Colheitadeira"
-                proprietario="Fazenda São João"
-                marca="John Deere"
+                key={item.id}
+                title={item.nome}
+                img={item.imagem}
+                preco={item.valor}
+                categoria={item.categoria}
+                proprietario={item.fazenda}
+                marca={item.marca}
               />
-            ))}
+            )
+          })}
         </div>
       </div>
     </section>
