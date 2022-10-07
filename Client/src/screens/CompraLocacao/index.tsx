a00c7b5b450935064e4104550f0d07ffd015985f
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2022-10-06 23:08:21 -0300

    :lipstick: feat Formas de pagamento

--- Client/src/screens/CompraLocacao/index.tsx
+++ Client/src/screens/CompraLocacao/index.tsx
@@ -1,7 +1,12 @@
 import './styles.css'
 import { AiOutlineFileSearch } from 'react-icons/ai'
 import imagem from '../../assets/img/trator.jpg'
-import { FaMapMarkerAlt, FaTrash } from 'react-icons/fa'
+import {
+  FaBarcode,
+  FaMapMarkerAlt,
+  FaRegCreditCard,
+  FaTrash,
+} from 'react-icons/fa'
 import { RiShoppingBag3Fill } from 'react-icons/ri'
 import { Link } from 'react-router-dom'
 
@@ -108,7 +113,7 @@ export default function CompraLocacao() {
           </div>
         </div>
       </div>
-      <div className="bg-white col-span-1 mt-4 ml-4 mb-96">
+      <div className="bg-white col-span-1 mt-4 ml-4 mb-72">
         <div>
           <h1 className="text-start ml-3 flex mt-8">
             <span>
@@ -157,7 +162,31 @@ export default function CompraLocacao() {
             </label>
           </span>
         </div>
-        <div className="m-5 rounded-md font-bold h-12 flex items-center justify-center ">
+
+        <div className="m-5 rounded-md font-bold h-24 flex items-center flex-col justify-center ">
+          <div className="w-full flex items-center justify-center">
+            <Link className="w-full px-1 py-1" to={'#'}>
+              <button className="flex items-center justify-center bg-white border-green-600 border-[1px]">
+                <FaBarcode color="#0f8239" className="h-12 w-12" />
+              </button>
+            </Link>
+            <Link className="w-full px-2 py-1" to={'#'}>
+              <button className="flex items-center justify-center bg-white border-green-600 border-[1px]">
+                <FaRegCreditCard color="#0f8239" className="h-12 w-12" />
+              </button>
+            </Link>
+            <Link className="w-full px-2 py-1" to={'#'}>
+              <button className="flex items-center justify-center bg-white fill-green-400 border-green-600 border-[1px]">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-12 w-12"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M242.4 292.5C247.8 287.1 257.1 287.1 262.5 292.5L339.5 369.5C353.7 383.7 372.6 391.5 392.6 391.5H407.7L310.6 488.6C280.3 518.1 231.1 518.1 200.8 488.6L103.3 391.2H112.6C132.6 391.2 151.5 383.4 165.7 369.2L242.4 292.5zM262.5 218.9C256.1 224.4 247.9 224.5 242.4 218.9L165.7 142.2C151.5 127.1 132.6 120.2 112.6 120.2H103.3L200.7 22.76C231.1-7.586 280.3-7.586 310.6 22.76L407.8 119.9H392.6C372.6 119.9 353.7 127.7 339.5 141.9L262.5 218.9zM112.6 142.7C126.4 142.7 139.1 148.3 149.7 158.1L226.4 234.8C233.6 241.1 243 245.6 252.5 245.6C261.9 245.6 271.3 241.1 278.5 234.8L355.5 157.8C365.3 148.1 378.8 142.5 392.6 142.5H430.3L488.6 200.8C518.9 231.1 518.9 280.3 488.6 310.6L430.3 368.9H392.6C378.8 368.9 365.3 363.3 355.5 353.5L278.5 276.5C264.6 262.6 240.3 262.6 226.4 276.6L149.7 353.2C139.1 363 126.4 368.6 112.6 368.6H80.78L22.76 310.6C-7.586 280.3-7.586 231.1 22.76 200.8L80.78 142.7H112.6z" />
+                </svg>
+              </button>
+            </Link>
+          </div>
           <button className="text-white bg-green-brasao text-center hover:bg-green-brasao2">
             Confirmar pagamento
           </button>
