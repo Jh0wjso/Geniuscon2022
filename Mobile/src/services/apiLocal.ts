145c6e670de8be4a0686b4ee4f7ab49f11ef7cd0
Felipe H. C. Ferreira <48157305+FelipeFerreiraDev@users.noreply.github.com>
2022-10-06 21:13:54 -0300

    Delete Mobile directory

--- Mobile/src/services/apiLocal.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const apiLocal = axios.create({
-  baseURL: 'https://backend-turismo-bandeirantes.herokuapp.com/api',
-  // baseURL: 'https://bband.discloud.app/api',
-})
-
-export default apiLocal
